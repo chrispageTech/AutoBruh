17cad8eec05fd54e347898ce68a9fbf26e3e08de
chrispageTech <71789492+chrispageTech@users.noreply.github.com>
2022-10-27 14:05:22 -0700

    Update AutoBruh.py

--- AutoBruh.py
+++ AutoBruh.py
@@ -1,16 +1,75 @@
-import pyautogui as pyg
-import pillow
 import tkinter as tk
+from tkinter import *
+import pyautogui as pyauto
+import PIL
+import time
 
-AutoBruh=AutoBruh.pyg
-Window= tk.Tk()
-Window.title('AutoBruh')
-button = tk.Button(AutoBruh, text='Get Mouse Pos', width=25, command='pyg.position()')
-button = tk.Button(Window, text='Setup', width=25, command='')
-button = tk.Button(Window, text='Save', width=25, command='')
-button = tk.Button(Window, text='Import', width=25, command='')
-button = tk.Button(Window, text='Start', width=25, command='')
-button = tk.Button(Window, text='Stop', width=25, command=Window.destroy)
-button.pack()
-Window.mainloop()
-pause
+print ("Loading AutoBruh!")
+print ("Loaded!")
+AutoBruh = tk.Tk()
+AutoBruh.title('AutoBruh')
+AutoBruh.geometry("150x200")
+
+coal = pyauto.locateOnScreen('coal.png')
+bank = pyauto.locateOnScreen('bank.png')
+depall = pyauto.locateOnScreen('dump_inventory.png')
+closebank = pyauto.locateOnScreen('close_bank.png')
+
+def AutoBruh_Close():
+    AutoBruh.destroy()
+def AutoBruh_GetPOS():
+        Xcordpos, Ycordpos = pyauto.position()
+        pyauto.alert(text=(Xcordpos, Ycordpos), title='Your Pos', button='Bruh')
+def MineCoal():
+    AmountCoal = 1
+    while AmountCoal < 10:
+        pyauto.click(1484, 561)
+        time.sleep(25)
+        pyauto.click(1512, 489)
+        time.sleep(1)
+        pyauto.click(1526, 578)
+        time.sleep(1)
+        pyauto.click(1640, 309)
+
+    AmountCoal += 1
+
+tk.Button(
+    AutoBruh,
+    text='GetMousePos',
+    command=AutoBruh_GetPOS
+).pack()
+tk.Button(
+    AutoBruh,
+    text='CoalBuddy',
+    command=MineCoal
+).pack()
+tk.Button(
+    AutoBruh,
+    text='Save',
+    command='AutoBruh.close'
+).pack()
+tk.Button(
+    AutoBruh,
+    text='Import',
+    command='AutoBruh.close'
+).pack()
+tk.Button(
+    AutoBruh,
+    text='Start',
+    command='AutoBruh.close'
+).pack()
+tk.Button(
+    AutoBruh,
+    text='Stop',
+    command='AutoBruh'
+).pack()
+
+tk.Button(
+    AutoBruh,
+    text='Exit',
+    command=AutoBruh_Close
+).pack()
+
+
+AutoBruh.mainloop()
+time.sleep(5)
